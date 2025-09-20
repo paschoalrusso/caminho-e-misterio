120771778074d1e561e94ea36bd2a07268cf7dec
paschoalrusso <paschoalrusso@gmail.com>
2025-09-20 11:37:24 -0300

    chore: remover experimental.appDir e adicionar 'Histórias contadas'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true }
-};
+  // Config padrão. Sem "experimental.appDir".
+}
+
 module.exports = nextConfig;
+
